8924333a537512211e0486b26e2a5642c9a417e6
nadraliev <nadraliev@gmail.com>
2017-10-29 11:51:44 +0300

    number of readers reading at the same time is now limited + number of writers can now differ from 1

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
-#define MAXTHREAD 10  /* define # readers */
+#define MAXTHREAD 5  /* define # readers */
+#define MAXREAD 5
+#define MAXWRITETHREAD 2
 
 
 void access_database();     /* prototypes */
-void non_access_database();
 
 void* reader(void*);
 void* writer(void*);
@@ -24,17 +27,22 @@ int main()
     int index;
     int ids[MAXTHREAD]; /* readers and initialize mutex, q and db-set them to 1 */
     sem_init (&q,0,1);
-    for(index = 0; index < MAXTHREAD; index ++)
-    {
+    for(index = 0; index < MAXTHREAD; index ++) {
         ids[index]=index+1;                             /* create readers and error check */
         if(pthread_create(&readers[index],0,reader,&ids[index])!=0){
             perror("Cannot create reader!");
             _exit(1);
         }
     }
-    if(pthread_create(&writerTh,0,writer,0)!=0){
-        perror("Cannot create writer");     /* create writers and error check */
-        _exit(1);
+
+    int writerIndex;
+    int writersIds[MAXWRITETHREAD];
+    for (writerIndex = 0; writerIndex < MAXWRITETHREAD; writerIndex++) {
+        writersIds[writerIndex] = writerIndex + 1;
+        if (pthread_create(&writerTh, 0, writer, &writersIds[writerIndex]) != 0) {
+            perror("Cannot create writer");     /* create writers and error check */
+            _exit(1);
+        }
     }
 
     pthread_join(writerTh,0);
@@ -44,24 +52,33 @@ int main()
 
 void* reader(void*arg)          /* readers function to read */
 {
+    srand(time(NULL));
     int index = *(int*)arg;
     int can_read;
-    while(1){
+    while(1) {
         can_read = 1;
 
+        announce_access_request();
+
         sem_wait(&q);
-        if(wc == 0 && write_request == 0)  rc++;
-        else                               can_read = 0;
+        if(wc == 0 && write_request == 0 && rc < MAXREAD) {
+            rc++;
+            printf("Reader %d wants to read | Resource is free\n", index);
+        }
+        else {
+            can_read = 0;
+            printf("Reader %d wants to read | Resource is busy\n", index);
+        }
         sem_post(&q);
 
         if(can_read) {
+            printf("Reader %d reading\n", index);
             access_database();
-            printf("Thread %d reading\n", index);
-            sleep(index);
 
             sem_wait(&q);
             rc--;
             sem_post(&q);
+            printf("Reader %d stopped reading\n", index);
         }
 
         sched_yield();
@@ -71,26 +88,35 @@ void* reader(void*arg)          /* readers function to read */
 ;
 void* writer(void*arg)      /* writer's function to write */
 {
+    srand(time(NULL));
+    int index = *(int*)arg;
     int can_write;
     while(1){
         can_write = 1;
-        non_access_database();
+
+        announce_access_request();
 
         sem_wait (&q);
-        if(rc == 0)   wc++;
-        else          { can_write = 0; write_request = 1; }
+        if(rc == 0 && wc == 0) {
+            wc++;
+            printf("Writer %d wants to write. | Resource is free\n", index);
+        }
+        else {
+            can_write = 0;
+            printf("Writer %d wants to write. | Number of readers %d\n", index, rc);
+        }
+        write_request++;
         sem_post(&q);
 
         if(can_write) {
+            printf("Writer %d is now writing\n",index);
             access_database();
-            printf("Writer is now writing...Number of readers: %d\n",rc);
-
-            sleep(3);
 
             sem_wait(&q);
             wc--;
-            write_request = 0;
+            write_request--;
             sem_post(&q);
+            printf("Writer %d stopped writing\n", index);
         }
 
         sched_yield();
@@ -98,13 +124,10 @@ void* writer(void*arg)      /* writer's function to write */
     return 0;
 }
 
-void access_database()
-{
-
+void announce_access_request() {
+    sleep(rand() % 21);
 }
 
-
-void non_access_database()
-{
-
+void access_database() {
+    sleep(rand() % 21);
 }
